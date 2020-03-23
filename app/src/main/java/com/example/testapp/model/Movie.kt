56c5d581c618307357683842c4291ea5c67d3c7f
Saswadkar, Jaimala <jsaswadkar@deloitte.com>
2020-03-23 14:33:12 +0530

    .gitignore updated
    login flow
    
    saving changes
    
    ui structure updated as per comment
    
    util class and launch activity update
    
    removing implementation not to be pushed

---
+++ app/src/main/java/com/example/testapp/model/Movie.kt
@@ -0,0 +1,5 @@
+package com.example.testapp.model
+
+class Movie {
+
+}
---
+++ app/src/main/java/com/example/testapp/ui/movie/MovieFragment.kt
@@ -0,0 +1,11 @@
+package com.example.testapp.ui.movie
+
+
+import androidx.fragment.app.Fragment
+
+
+class MovieFragment : Fragment() {
+
+
+
+}
